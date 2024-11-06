00a196b6b6d5a285a1e76720a349da9ad5ec4264
stefanbgh <stefanbgh.po@gmail.com>
2024-11-06 18:54:38 +0100

    update 404 page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,61 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import img from "../assets/images/404.png";
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+	return (
+		<>
+			<Stack.Screen options={{ title: "Oops!" }} />
+			<View style={localStyles.container}>
+				<Image source={img} style={localStyles.image} />
+				<View style={{ marginTop: 5, marginBottom: 30 }}>
+					<Text style={localStyles.message}>Oops!!</Text>
+					<Text style={localStyles.desc}>
+						It seems the page you're looking for doesn't exist.
+					</Text>
+				</View>
+				<Link href="/" style={localStyles.button}>
+					<Text style={localStyles.buttonText}>Back to App</Text>
+				</Link>
+			</View>
+		</>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+const localStyles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 20,
+	},
+	image: {
+		width: 125,
+		height: 125,
+		marginBottom: 10,
+		transform: "rotate(-5deg)",
+	},
+	message: {
+		fontSize: 24,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	desc: {
+		fontSize: 20,
+		textAlign: "center",
+	},
+	button: {
+		backgroundColor: "#446bac",
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		borderRadius: 10,
+		marginTop: 5,
+		alignSelf: "center",
+	},
+	buttonText: {
+		color: "white",
+		fontSize: 18,
+		fontWeight: "bold",
+	},
 });
